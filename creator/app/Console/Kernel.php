2c5d4c7b78481ff8623ff2d1cabc3db066643420
Jesús Amieiro <amieiro@gmail.com>
2020-07-01 13:03:23 +0000

    Add the creation command to the Laravel scheduler

--- creator/app/Console/Kernel.php
+++ creator/app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('ded:create-files')->dailyAt('5:00');;
     }
 
     /**
