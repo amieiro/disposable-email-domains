5e1486af3c094ed28ba514c48636c4cd37cdcc36
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-10-29 08:14:08 +0100

    Add the file from the Dahoom152/disposable-email project
    https://raw.githubusercontent.com/Dahoom152/disposable-email/main/domains.json

--- creator/app/Console/Commands/CreateDisposableEmailDomainsFilesCommand.php
+++ creator/app/Console/Commands/CreateDisposableEmailDomainsFilesCommand.php
@@ -30,6 +30,7 @@ class CreateDisposableEmailDomainsFilesCommand extends Command
     ];
 
     protected $jsonDenyFiles = [
+        'https://raw.githubusercontent.com/Dahoom152/disposable-email/main/domains.json',
         'https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json',
         'https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/wildcard.json',
         'https://raw.githubusercontent.com/Propaganistas/Laravel-Disposable-Email/master/domains.json',
